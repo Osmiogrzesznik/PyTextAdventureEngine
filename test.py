53bb6395cb854d519130c96cae7d17ed25d0dfc8
Osmiogrzesznik <boleskok@gmail.com>
2020-07-02 21:46:06 +0100

    attempts doing the tests the right way?

--- test.py
+++ test.py
@@ -19,23 +19,37 @@ scenario = {
     # check why it happens and make sure the object returned is always objectinfo
     'open door': 0,
     'pick the lock': lambda game: game.current.dct['exits']['door']['open'],
-
+    'go door': lambda game: game.current.name != 'asylum cell'
 }
 
 
 class TestGameScenario(unittest.TestCase):
-    def test_scenario()
+    def setUp(self):
+        self.mapp = deepcopy(map)
+        self.scenario = {
+            'wake up': 0,
+            'look bed': 0,
+            'look pillow': 0,
+            'take pillow': lambda game: 'wire' in game.current.dct['items'],
+            # displays cannot take pilow yet wire is present in items
+            'take wire': lambda game: game.playerHas('wire'),
+            # what is returned from game_init line 216 obinfo is an actual object ont an info,
+            # check why it happens and make sure the object returned is always objectinfo
+            'open door': 0,
+            'pick the lock': lambda game: game.current.dct['exits']['door']['open'],
+            'go door': lambda game: game.current.name != 'asylum cell'
+        }
+        self.textUI = testUserInterface()
+        self.game = Game(self.mapp, self.textUI, None, None)
+
+    def test_scenario(self):
+        for cm in self.scenario:
+            self.game.handleUserInput(cm)
+            if not self.scenario[cm]:
+                continue
+
+            self.assertTrue(scenario[cm](self.game))
 
 
-for cm in scenario:
-
-    print('TEST SCRIPT ENTERED COMMAND:' + cm)
-    game.handleUserInput(cm)
-    if scenario[cm]:
-        if scenario[cm](game):
-            working = 'working'
-        else:
-            working = 'not working'
-        print('TEST:\'' + cm + '\' command is ' + working)
-
-print('TEST HAS ENDED')
+if __name__ == "__main__":
+    unittest.main()
